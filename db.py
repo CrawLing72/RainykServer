5cfe51cf96ae55859938094767edb63042d30f3b
CrawLing72 <caseus.molamola@gmail.com>
2024-09-25 18:56:55 +0900

    FETCH : DB

--- db.py
+++ db.py
@@ -3,16 +3,21 @@ import sqlite3
 con_user = sqlite3.connect("./user.db")
 cur_user = con_user.cursor()
 
-cur_user.execute("CREATE TABLE user(UID INTEGER PRIMARY KEY, name TEXT)")
+cur_user.execute("CREATE TABLE user(UID INTEGER PRIMARY KEY, name TEXT, password TEXT)")
 con_user.commit()
 
 
-def search_user_id(user_name: str):
+def search_user_id(user_name: str) -> int:
     cur_user.execute("SELECT * FROM user WHERE name like ?", str)
-    result = cur_user.fetchall()
+    result = cur_user.fetchone()
     print(result)
+    return int(result)
 
 
-def get_user_id(user_name: str):
-    return
+def insert_user_id(user_name: str, uid: str) -> None:
+    cur_user.execute("INSERT INTO user VALUES (?, ?)", (uid, user_name))
+    con_user.commit()
+
+
+
 
