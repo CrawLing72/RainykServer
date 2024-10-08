c4051ec60b36ab4c798e33b351e781398afc75a2
Huidong Shin <dongdongcompany@khu.ac.kr>
2024-10-08 20:46:45 +0900

    UPDATE : Port (80 -> 5000)

--- app.py
+++ app.py
@@ -67,4 +67,4 @@ def gameover():
     return jsonify(answert="completed!"), 200
 
 if __name__ == '__main__':
-    app.run( port=80, debug= True, host = '0.0.0.0')
+    app.run( port=5000, debug= True, host = '0.0.0.0')
